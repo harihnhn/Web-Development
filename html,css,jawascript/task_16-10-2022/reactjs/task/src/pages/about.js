5f3d735bb7ce2ecabfd0c067290385e5ab16b380
vetri <vetrivendanucet@gmail.com>
2022-10-28 18:39:30 +0530

    oct-28, 2022 commit

---
+++ html,css,jawascript/task_16-10-2022/reactjs/task/src/pages/about.js
@@ -0,0 +1,14 @@
+import React from "react";
+//import about from "./../images/" 
+
+class About extends React.Component{
+    render(){
+        return(
+            <div>
+                <h1>welcome to about page</h1>
+            </div>
+        );
+    }
+}
+
+export default About;
